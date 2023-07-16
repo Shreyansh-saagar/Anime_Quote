db56524b1efb931926e663f1601644ab44303a5d
Shreyansh saagar <Shreyanshsaagar@gmail.com>
2023-07-16 23:27:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,28 +4,25 @@ const container = () =>{
         <div class="box">
             <div class="icon">
                 <div class="iconbox">
-                    <lord-icon
-                    src="https://cdn.lordicon.com/wloilxuq.json"
-                    trigger="hover"
-                    colors="primary:#ffffff,secondary:#ffffff"
-                    state="hover-2">
-                    </lord-icon>
                 </div>
             </div>
             <div class="content">
                 <h3 id="heading"></h3>
+                <div id="scroll">
                 <p id="quote"></p>
+                <div>
             </div>
         </div>
     </div>`
 }
 const animepromise = async (name) =>{
-    let p = await fetch("https://animechan.xyz/api/random/anime?title="+ name)
+    let p = await fetch("https://animechan.xyz/api/random/anime?title="+name)
     let response =  p.json()
     return response
 }
 const animeQuote = async ()=>{
-    let a = await animepromise("Naruto")
+    let pro = prompt("Enter Anime name:")
+    let a = await animepromise(pro)
     console.log(a)
     container()
     document.getElementById("heading").innerHTML= a.character
